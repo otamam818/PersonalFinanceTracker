eef97c7ed8ab069e4ef8ac825169bbe220f9371a
otamam818 <otamam818@gmail.com>
2022-04-01 16:15:34 +1100

    Added main GUI window (without any widgets)

--- GUI/main.py
+++ GUI/main.py
@@ -1,7 +1,20 @@
 import sys
+from PySide6.QtWidgets import QApplication, QWidget, QHBoxLayout
 
 def main(args): 
-    print(f"Hwllo {args[1]}")
+    run_app()
+
+class FinanceTracker(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+def run_app():
+    app = QApplication(sys.argv)
+
+    myFinTracker = FinanceTracker()
+    myFinTracker.show()
+
+    sys.exit(app.exec())
 
 if __name__ == "__main__":
     main(sys.argv)
