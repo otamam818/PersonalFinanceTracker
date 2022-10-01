227efd4737b898994a1c087ce60840f57ae4d80b
otamam818 <otamam818@gmail.com>
2022-10-01 16:00:38 +1000

    Removed java CLI directory

--- GUI/body_gui.py
+++
@@ -1,7 +0,0 @@
-import process_handler
-from PySide6.QtWidgets import QWidget
-
-class BodyWidget(QWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-
--- GUI/temp_input.py
+++
@@ -1,9 +0,0 @@
-def main(): 
-    choice = input("Enter your name: ")
-    if choice == "1":
-        print("Great Job!")
-    print("Hello World!")
-
-if __name__ == "__main__":
-    main()
-
