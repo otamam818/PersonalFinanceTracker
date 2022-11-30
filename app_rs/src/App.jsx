14c86659fff078c9cfb94bac7ec3d1d7e72e6fa8
otamam818 <otamam818@gmail.com>
2022-12-01 00:07:56 +0600

    Added styles and implemented welcome page

--- app_rs/src/App.jsx
+++ app_rs/src/App.jsx
@@ -1,11 +1,25 @@
+import {useState} from "react";
 import "./App.scss";
+import WelcomePage from "./WelcomePage/index";
 
 function App() {
+  let [currConfig, setConfig] = useState({ loadPath : "", name : "Oisho" });
+  let [componentChoice, setComponent] = useState("welcome");
+  currConfig = {...currConfig, setConfig, setComponent};
+
   return (
-    <div className="container">
-    Hello Personal Finance Tracker
+    <div className="main-body">
+      {chooseCurrComponent(componentChoice, currConfig)}
     </div>
   );
+}
+
+function chooseCurrComponent(componentChoice, currConfig) {
+  switch (componentChoice) {
+    case "welcome" : return <WelcomePage currConfig={currConfig} />;
+    case "loadFile" : return <div>Hello loadFile</div>;
+    default: return (<div>404, not found</div>)
+  }
 }
 
 export default App;
