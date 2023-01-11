e8d927a4c5c4cb41c21dfba49237146278f74b9d
otamam818 <otamam818@gmail.com>
2023-01-12 10:26:30 +1100

    Changed front-end implementation to suit the new data-structure(s)

--- app_rs/src/Dashboard/dataHandler.js
+++ app_rs/src/Dashboard/dataHandler.js
@@ -1,9 +1,12 @@
 
 export function allEmpty(userData) {
-  return Object.keys(userData.bought_items).length === 0 &&
-         Object.keys(userData.category).length     === 0 &&
-         Object.keys(userData.items).length        === 0 &&
-         Object.keys(userData.receipts).length     === 0 &&
-         Object.keys(userData.stores).length       === 0 ;
+  // NOTE: Data handling should be done by rust whenever possible, where this
+  //       function happens to make better sense in rust anyways
+  // TODO: Port this to rust
+  return userData.bought_items === null &&
+         userData.category === null &&
+         userData.items === null &&
+         userData.receipts === null &&
+         userData.stores === null;
 }
 
