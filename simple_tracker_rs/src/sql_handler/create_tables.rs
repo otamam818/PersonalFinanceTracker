ae2cd1f0f925c433aff0a39069eb2aa37be1df14
MT <04x0xx@gmail.com>
2024-01-02 19:23:48 -0400

    Updated ERD and added foreign keys to tables

--- simple_tracker_rs/src/sql_handler/create_tables.rs
+++ simple_tracker_rs/src/sql_handler/create_tables.rs
@@ -11,17 +11,21 @@ pub async fn execute(pool: &mut SqliteConnection) -> Result<(), DynamicError> {
     )").execute(conn).await?;
 
     let conn = pool.acquire().await?;
+    sqlx::query("CREATE TABLE IF NOT EXISTS category (
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL
+    );").execute(conn).await?;
+
+    let conn = pool.acquire().await?;
     sqlx::query("CREATE TABLE IF NOT EXISTS item (
         id INTEGER PRIMARY KEY,
         name TEXT NOT NULL,
         category_id INTEGER,
-        current_price REAL NOT NULL
-    );").execute(conn).await?;
-
-    let conn = pool.acquire().await?;
-    sqlx::query("CREATE TABLE IF NOT EXISTS category (
-        id INTEGER PRIMARY KEY,
-        name TEXT NOT NULL
+        current_price REAL NOT NULL,
+        FOREIGN KEY category_id
+            REFERENCES category(id)
+                ON UPDATE CASCADE
+                ON DELETE CASCADE
     );").execute(conn).await?;
 
     // Represents an item unit, such as kg, ml, L, per-each, etc.
@@ -37,41 +41,69 @@ pub async fn execute(pool: &mut SqliteConnection) -> Result<(), DynamicError> {
         id INTEGER PRIMARY KEY,
         from_id INTEGER,
         to_id INTEGER,
-        multiplier REAL NOT NULL
+        multiplier REAL NOT NULL,
+        FOREIGN KEY from_id
+            REFERENCES unit(id)
+                ON UPDATE CASCADE
+                ON DELETE CASCADE,
+        FOREIGN KEY to_id
+            REFERENCES unit(id)
+                ON UPDATE CASCADE
+                ON DELETE CASCADE
     )").execute(conn).await?;
 
     let conn = pool.acquire().await?;
     sqlx::query("CREATE TABLE IF NOT EXISTS past_prices (
         id INTEGER PRIMARY KEY,
         item_id INTEGER,
-        price REAL NOT NULL
+        price REAL NOT NULL,
+        FOREIGN KEY item_id
+            REFERENCES item(id)
+                ON UPDATE CASCADE
+                ON DELETE CASCADE
     )").execute(conn).await?;
 
-    let conn = pool.acquire().await?;
     // TODO: Make location become a separate table called `branches` since a
     // venue can have multiple branches
-    sqlx::query("CREATE TABLE IF NOT EXISTS venue (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        location TEXT
-    )").execute(conn).await?;
 
     let conn = pool.acquire().await?;
     sqlx::query("CREATE TABLE IF NOT EXISTS receipt (
         id INTEGER PRIMARY KEY,
         venue_id INTEGER,
-        date TEXT
+        date TEXT,
+        FOREIGN KEY venue_id
+            REFERENCES venue(id)
+                ON UPDATE CASCADE
+                ON DELETE CASCADE
     )").execute(conn).await?;
 
     let conn = pool.acquire().await?;
     sqlx::query("CREATE TABLE IF NOT EXISTS receipt_items (
         id INTEGER PRIMARY KEY,
-        receipt_id INTEGER,
-        item_id INTEGER,
+        receipt_id INTEGER NOT NULL,
+        item_id INTEGER NOT NULL,
+        unit_id INTERGER NOT NULL,
         -- how many did the user buy at this time
-        quantity INTEGER NOT NULL
+        quantity INTEGER NOT NULL,
+
+        FOREIGN KEY receipt_id
+            REFERENCES receipt(id)
+                ON UPDATE CASCADE
+                ON DELETE CASCADE,
+ 
+        FOREIGN KEY item_id
+            REFERENCES item(id)
+                ON UPDATE CASCADE
+                ON DELETE CASCADE,
+
+        FOREIGN KEY unit_id
+            REFERENCES unit(id)
+                ON UPDATE CASCADE
+                ON DELETE CASCADE
     )").execute(conn).await?;
 
+    // Id reccomend removing this, it doesn't seem to do anything
+    // the receipt_items table can't (not included in ERD)
     let conn = pool.acquire().await?;
     sqlx::query("CREATE TABLE IF NOT EXISTS receipt_items_quantities (
         id INTEGER PRIMARY KEY,
